060adbace1a5162064bbdc0e71d473010c9e5834
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2022-12-18 23:22:55 +0000

    Add input item

--- run.lua
+++ run.lua
@@ -2,7 +2,8 @@ dofile("cc_storage/utils/hooks.lua")
 dofile("cc_storage/utils/timer.lua")
 dofile("cc_storage/storage/items.lua")
 
-local items = storage.getItemMapping(storage.getChests())
-storage.getItem("minecraft:bone", 5)
+storage.updateChests()
+storage.updateItemMapping()
+storage.dropItem("minecraft:bone", 5)
 
 hook.runLoop()
--- storage/items.lua
+++ storage/items.lua
@@ -1,12 +1,13 @@
 dofile("cc_storage/utils/helpers.lua")
+dofile("cc_storage/utils/timer.lua")
 
 storage = {}
 
-function storage.getChests()
-  return {peripheral.find("minecraft:chest")}
+function storage.updateChests()
+  storage.chests = {peripheral.find("minecraft:chest")}
+  storage.dropper = peripheral.find("minecraft:trapped_chest")
+  storage.input = peripheral.find("minecraft:barrel")
 end
-
-storage.dropper = peripheral.find("minecraft:trapped_chest")
 
 --[[
 key = itemName + nbt + durability
@@ -23,27 +24,42 @@ items = {
 }
 ]]
 
-function storage.getItemMapping(chests)
-  local items = {}
-
-  for _, chest in pairs(chests) do
-    for k, item in pairs(chest.list()) do
-      local detail = chest.getItemDetail(k)
-
-      local key = item.name .. (detail.nbt or "") .. (detail.damage or "")
-
-      items[key] = items[key] or {count = 0, locations = {}}
-      items[key].count = items[key].count + item.count
-      items[key].detail = detail
-      table.insert(items[key].locations, {chest = chest, slot = k, count = item.count})
-    end
-  end
-
-  return items
+function storage.getItemKey(item, detail)
+  return item.name .. (detail.nbt or "") .. (detail.damage or "")
 end
 
-function storage.dropItem(items, key, count)
-  local item = items[key]
+function storage.saveItem(item, detail, chest, slot)
+  local items = storage.items
+  local key = storage.getItemKey(item, detail)
+
+  items[key] = items[key] or {count = 0, locations = {}}
+  items[key].count = items[key].count + item.count
+  items[key].detail = detail
+  table.insert(items[key].locations, {chest = chest, slot = slot, count = item.count})
+end
+
+function storage.updateItemMapping(chests)
+  storage.items = {}
+  storage.emptySlots = {}
+  local items = storage.items
+
+  for _, chest in pairs(chests) do
+    local chestItems = chest.list()
+    for k = 1, chest.size() do
+      local item = chestItems[k]
+      if item then
+        local detail = chest.getItemDetail(k)
+
+        storage.saveItem(item, detail, chest, k)
+      else
+        table.insert(storage.emptySlots, {chest = chest, slot = k})
+      end
+    end
+  end
+end
+
+function storage.dropItem(key, count)
+  local item = storage.items[key]
   if not item then
     return false
   end
@@ -51,7 +67,7 @@ function storage.dropItem(items, key, count)
   storage.dropItems(item.locations, count)
   item.count = item.count - count
   if item.count <= 0 then
-    items[keys] = nil
+    storage.items[keys] = nil
   end
   return true
 end
@@ -77,6 +93,60 @@ function storage.dropItems(locations, count)
       break
     else
       table.remove(locations, 1)
+      table.insert(storage.emptySlots, {chest = location.chest, slot = location.slot})
+    end
+  end
+end
+
+function storage.startInputTimer()
+  timer.create("input", 0.5, 0, storage.inputLoop)
+end
+
+function storage.inputLoop()
+  local inputItems = storage.input.list()
+  if #inputItems == 0 then
+    return
+  end
+
+  if #storage.emptySlots == 0 then
+    print("OUT OF SPACEEEEEE")
+    return
+  end
+
+  for k, item in pairs(inputItems) do
+    storage.inputItem(k, item)
+  end
+end
+
+function storage.inputItem(slot, item)
+  local detail = storage.input.getItemDetail(slot)
+  local key = storage.getItemKey(item, detail)
+  local storedItem = storage.items[key]
+  if storedItem then
+    storage.inputItems(storedItem, slot, item)
+  end
+  if item.count == 0 then return end
+
+  if #storage.emptySlots == 0 then return end
+  local newSlot = table.remove(storage.emptySlots, 1)
+  storage.input.pushItems(peripheral.getName(newSlot.chest), slot, item.count, newSlot.slot)
+
+  storage.saveItem(item, detail, newSlot.chest, newSlot.slot)
+end
+
+function storage.inputItems(item, slot, newItem)
+  local max = item.detail.maxItems
+  for _, location in ipairs(item.locations) do
+    local canAdd = max - location.count
+    if canAdd > 0 then
+      local toMove = canAdd
+      if canAdd >= newItem.count then
+        toMove = newItem.count
+      end
+      storage.input.pushItems(peripheral.getName(location.chest), slot, toMove, location.slot)
+      location.count = location.count + toMove
+      newItem.count = newItem.count - toMove
+      if newItem.count == 0 then break end
     end
   end
 end
