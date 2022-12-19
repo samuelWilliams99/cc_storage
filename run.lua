68fbb25b7e5dbb45072fad12c426b8e262b1d399
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2022-12-19 21:41:22 +0000

    Fix simple items ui

--- run.lua
+++ run.lua
@@ -16,11 +16,11 @@ term.clear()
 local counter = 0
 for name, item in pairs(storage.items) do
   counter = counter + 1
-  if counter > 36 then break end
+  if counter > 30 then break end
 
   local button = ui.button.create()
-  button:setPos(counter, 2)
-  button:setSize(90, 1)
+  button:setPos(2, counter)
+  button:setSize(80, 1)
   button:setText(item.detail.displayName .. ": " .. item.count)
   button:setBgColor(counter % 2 == 0 and colors.gray or colors.black)
   function button:onClick()
