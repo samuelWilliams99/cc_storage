d953b8293e883238170275b4280ea44a3498caf7
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2022-12-19 21:03:59 +0000

    Clear hooks

--- utils/hooks.lua
+++ utils/hooks.lua
@@ -1,19 +1,24 @@
 if hook then return end
 
 hook = {}
+hook.handlers = {}
 
 function hook.add(eventName, handlerName, handler)
-    hook[eventName] = hook[eventName] or {}
-    hook[eventName][handlerName] = handler
+    hook.handlers[eventName] = hook.handlers[eventName] or {}
+    hook.handlers[eventName][handlerName] = handler
 end
 
 hook.run = os.queueEvent
+
+function hook.clear()
+    hook.handlers = {}
+end
 
 function hook.runLoop()
     os.queueEvent("initialize")
     while true do
         local data = {os.pullEvent()}
-        local handlerTable = hook[data[1]]
+        local handlerTable = hook.handlers[data[1]]
         table.remove(data, 1)
 
         for _, handler in pairs(handlerTable or {}) do
