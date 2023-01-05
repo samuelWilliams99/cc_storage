409651f95a51d45117913cc13512a012e5616fec
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2023-01-05 21:10:13 +0000

    fixes

--- ui/pages/recipes.lua
+++ ui/pages/recipes.lua
@@ -16,7 +16,7 @@ function recipesPage.setup()
 
   local backButton = addElem(ui.text.create())
   backButton:setPos(2, h - 4)
-  backButton:setSize(11, 3)
+  backButton:setSize(10, 3)
   backButton:setTextDrawPos(3, 1)
   backButton:setText("Back")
   function backButton:onClick()
@@ -25,7 +25,7 @@ function recipesPage.setup()
 end
 
 function recipesPage.cleanup()
-  for _, elem in ipairs(craftCountPage.elems) do
+  for _, elem in ipairs(recipesPage.elems) do
     elem:remove()
   end
 end
